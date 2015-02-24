e5e0810355e39368840c93e973f41eead92a0a80
Fredrik Sävje <fred_gh@savje.com>
2015-02-24 15:57:34 +0100

    Make development environment

---
+++ R/appopt.R
@@ -0,0 +1,9 @@
+#' appopt: An approximate optimal threshold blocking algorithm
+#'
+#' An approximate optimal threshold blocking algorithm
+#' TODO: write real description
+#'
+#' @docType package
+#' @name appopt-package
+#' @aliases appopt
+NULL
